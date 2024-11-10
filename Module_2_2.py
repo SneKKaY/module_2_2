49f2bd5c8cd646f8fc82d17eff26785be764a7c7
SneKKaY <120265382+SneKKaY@users.noreply.github.com>
2024-11-10 20:33:15 +0300

    Add files via upload

---
+++ Module_2_2.py
@@ -0,0 +1,15 @@
+# name = input("What's your name? ")
+# if name == "Dima" :
+#     print("Hey, smart-ass!")
+# else:
+#     print('Hello, ', name)
+
+first = int(input('Write number: '))
+second = int(input('Yet: '))
+third = int(input('Give me more number!: '))
+if first == second and third:
+    print(3)
+elif first == second or third:
+    print(2)
+else:
+    print(0)
